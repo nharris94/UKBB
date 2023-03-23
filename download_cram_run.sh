02c6187f6b88ce1244eb80bbdb0466ce576fc929
Natasha Harris <49454215+nharris94@users.noreply.github.com>
2023-03-23 11:25:10 -0400

    Add files via upload

---
+++ download_cram_run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+i=0
+input="bulk"${1}"cram.txt"
+while read path
+do
+    cram_name=$(basename "$path" .cram)
+    echo $cram_name
+
+    dx download Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/10/"${cram_name}".cram -o cram/
+    dx download Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/10/"${cram_name}".cram.crai -o cram/
+
+    bash run.tryptase.sh $cram_name
+
+    dx upload -r genotype/${cram_name} --path /Analysis_1/genotype/${cram_name}
+
+    rm cram/${cram_name}.*
+    #((i=i+1))
+    #if [[ $i == 5 ]]
+    #then
+    #    break
+    #fi
+done < $input
