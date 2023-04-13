545d3e87ba71c342cb881cf6f00a3749e0e67c49
Natasha Harris <49454215+nharris94@users.noreply.github.com>
2023-04-13 14:06:29 -0400

    updated mean_cov_geno

--- mean_cov_geno.sh
+++ mean_cov_geno.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 ls /mnt/project/Analysis_1/genotype/ > cram_run.txt
-echo "IID   MeanCoverage" > tryptase_test.sample.tsv
+echo "IID\tMeanCoverage" > tryptase_test.sample.tsv
+samtools="/usr/local/bin/samtools"
 
-for $cram in $(cat cram_run.txt)
+for cram in $(cat cram_run.txt)
 do
-    dx download Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/*/"${cram}".cram -o cram/
-    dx download Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/*/"${cram}".cram.crai -o cram/
+    dx download Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/10/"${cram}".cram -o cram/
+    dx download Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/10/"${cram}".cram.crai -o cram/
 
-    samtools depth cram/${cram} | awk '{sum+=$3} END { print "${cram}   ",sum/NR}' >> tryptase_test.sample.tsv
+    $samtools depth cram/${cram}.cram | awk -v cram_var="$cram" '{sum+=$3} END { print cram_var,"\t",sum/NR}' >> tryptase_test.sample.tsv
 
-    awk '{print "${cram}   ",$0}' >> tryptase_test.genotype.tsv
+    #awk cram_var="$cram" '{print cram_var,"\t",$0}' /mnt/project/Analysis_1/genotype/${cram}/${cram}.output.txt >> tryptase_test.genotype.tsv
 done
