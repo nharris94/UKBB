ea399bb61804a1bcd628bc3a63a85057d87f7515
Natasha Harris <49454215+nharris94@users.noreply.github.com>
2023-09-12 14:22:22 -0400

    Update mean_cov_geno.sh

--- mean_cov_geno.sh
+++ mean_cov_geno.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-ls /mnt/project/Analysis_1/genotype/ > cram_run.txt
-echo -e "IID\tMeanCoverage" > tryptase_test.sample.tsv
-samtools="/usr/local/bin/samtools"
-i=0
+#echo -e "IID\tMeanCoverage" > tryptase_${1}.sample.tsv
+input="bulk"${1}"cram_new.txt"
 
-for cram in $(cat cram_run.txt)
+while read path
 do
-    dx download Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/10/"${cram}".cram -o cram/
-    dx download Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/10/"${cram}".cram.crai -o cram/
+        cram_name=$(basename "$path" .cram)
+        
+        cat tsv/${cram_name}.sample.tsv >> tryptase_${1}.sample.tsv
+        cat tsv/${cram_name}.genotype.tsv >> tryptase_${1}.genotype.tsv
 
-    $samtools depth cram/${cram}.cram | awk -v cram_var="$cram" 'OFS="\t" {sum+=$3} END { print cram_var,sum/NR}' >> tryptase_test.sample.tsv
-
-    awk -v cram_var="$cram" 'OFS="\t" {print cram_var,$0}' /mnt/project/Analysis_1/genotype/${cram}/${cram}.output.txt >> tryptase_test.genotype.tsv
-    
-    rm cram/${cram}.*
-    
-    #after running certain number of jobs, terminate
-    #((i=i+1))
-    #if [[ $i == 5 ]]
-    #then
-    #    break
-    #fi
-done
+done < $input
